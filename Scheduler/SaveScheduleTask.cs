0cafed2ee6059f7b5f6f3ca4c94bfcfd8118604b
Mungert69 <43328293+Mungert69@users.noreply.github.com>
2020-06-02 20:59:32 +0100

    fixed save schedule

--- Scheduler/SaveScheduleTask.cs
+++ Scheduler/SaveScheduleTask.cs
@@ -24,7 +24,7 @@ namespace ASPNETCoreScheduler.Scheduler
         {
         }
 
-        protected override string Schedule => "* */2 * * *";
+        protected override string Schedule => "* * */2 * *";
 
         public override Task ProcessInScope(IServiceProvider serviceProvider)
         {
